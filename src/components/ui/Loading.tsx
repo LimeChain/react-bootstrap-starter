cee0e0f36334388ad5484c6dd3beaf432b7abdec
Martin Dobrev <martindobrev87@gmail.com>
2025-01-20 14:20:43 +0200

    feat: .cursorrules files added, api client added

---
+++ src/components/ui/Loading.tsx
@@ -0,0 +1,11 @@
+const Loading = () => {
+  return (
+    <div className="d-flex justify-content-center my-5 my-md-10">
+      <div className="spinner-grow text-primary" role="status">
+        <span className="visually-hidden">Loading...</span>
+      </div>
+    </div>
+  );
+};
+
+export default Loading;
